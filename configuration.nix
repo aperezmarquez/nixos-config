31cb518fa5dd134f6478268852a282b206e7d9cd
aperezmarquez <aperezmarquez.business@gmail.com>
2026-01-19 18:52:53 +0100

    Update: Full functional NixOS config with niri

--- configuration.nix
+++ configuration.nix
@@ -1,30 +1,28 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfree = true;
+
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos-conf"; # Define your hostname.
+  networking.hostName = "nixos-inst"; # Define your hostname.
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
+  
+  security.pki.certificateFiles = [
+    "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+  ];
 
   # Set your time zone.
-  time.timeZone = "Europe/Spain";
+  time.timeZone = "Europe/Madrid";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -33,34 +31,15 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "es_ES.UTF-8";
   console.keyMap = "es";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver = {
-  #   enable = true;
-  #   windowManager.qtile.enable = true;
-  # };
-  programs.niri.enable = true;
-
-  services.displayManager.gdm = {
-    wayland.enable = true;
-    enable = true;
-  };
-
-  programs.sway.enable = true;
   
-  environment.sessionVariables = {
-    XKB_DEFAULT_LAYOUT = "es";
-  };
-
   hardware.graphics = {
     enable = true;
   };
+
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
+
+  programs.niri.enable = true;
 
   services.xserver.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
@@ -71,48 +50,42 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "es";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  services.xserver.xkb.layout = "es";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.antonio = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
     ];
+    shell = pkgs.zsh;
   };
 
-  # Install firefox
-  programs.firefox.enable = true;
+  # STOPPED USING FIREFOX, NOW SELLING AND DISTRIBUTING YOUR DATA + NEARLY UNIQUE FINGERPRINT
+  # programs.firefox.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
+  # Desktop fonts
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    font-awesome
+    meslo-lgs-nf
+  ];
+  
+  # All the system Pkgs
   environment.systemPackages = with pkgs; [
-    i3
+    cacert
     vulkan-loader
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     neovim
+    git
+    gh
     kitty
-    ironbar
+    waybar
     eww
-    swaybg
     glibc
     wayland
     wayland-protocols
@@ -120,9 +93,9 @@
     libdrm
     libxkbcommon
     libxcursor
-    libx11
+    libvirt
+    libgcc
     pixman
-    git
     meson
     ninja
     libdisplay-info
@@ -130,20 +103,37 @@
     hwdata
     seatd
     pcre2
-    xcursor-themes
     xwayland-satellite
+    zsh-powerlevel10k
+    swww
+    jq
+    btop
+    stow
+    _7zz
+    brave
+    libreoffice
+    kdePackages.okular
   ];
+  
+  # Enabling zsh as a shell
+  programs.zsh.enable = true;
+  programs.zsh.promptInit = ''
+    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+  '';
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Set wallpaper on boot
+  systemd.user.services.wallpaper = {
+    description = "Set wallpaper for niri";
+    after = [ "graphical-session.target" ];
 
-  # List services that you want to enable:
+    serviceConfig = {
+      ExecStart = "${pkgs.swww}/bin/swww-daemon & sleep 1 & ${pkgs.swww}/bin/swww img -o eDP-1 /home/antonio/Images/Wallpapers/stars.jpg";
+      Restart = "on-failure";
+    };
 
+    wantedBy = [ "graphical-session.target" ];
+  };
+  
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
@@ -158,24 +148,8 @@
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  # Do NOT change this line, could BREAK  the system
+  system.stateVersion = "25.11";
 
 }
 
